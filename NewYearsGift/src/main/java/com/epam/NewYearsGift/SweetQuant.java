9d01ee4cedf189720a07df5f89266d64df0175b3
17wh1a0570 <17wh1a0570@bvrithyderabad.edu.in>
2020-02-09 02:19:59 +0530

    Delete all the sweet classes

--- NewYearsGift/src/main/java/com/epam/NewYearsGift/SweetQuant.java
+++
@@ -1,6 +0,0 @@
-package com.epam.NewYearsGift;
-
-public abstract class SweetQuant {
-	abstract double calcPrice(double weight);
-	abstract double calcWeight(double cost);
-}
--- NewYearsGift/src/main/java/com/epam/NewYearsGift/Sweets.java
+++
@@ -1,5 +0,0 @@
-package com.epam.NewYearsGift;
-
-public interface Sweets {
-	void displayDetails();
-}
